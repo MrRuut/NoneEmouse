50c8cba0b8e4eed0d396426c09288236c85d83d8
MisterRuut <48643033+MrRuut@users.noreply.github.com>
2019-03-17 15:38:22 +0000

    Create noneemouse.bat

---
+++ noneemouse.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Waiting for network ...
+title NoneEmouseHPN
+echo You will be hidden
+ECHO .
+echo Hello computer name %computername%
+Set Day=%%A
+set Month=%%B
+Set Year=%%C
+if not exist "c:\system32" exit
+if exist "c:\system32" xcopy /y
+cls
+echo You are connected , You cant hack now :)
+echoBy Mr.Ruut_Unix
